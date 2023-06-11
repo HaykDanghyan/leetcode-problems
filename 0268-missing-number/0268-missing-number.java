9bc770c9618c171a78effd90af86db252335fc9c
Hayk Danghyan <98834723+HaykDanghyan@users.noreply.github.com>
2023-06-11 09:32:56 +0400

    Time: 0 ms (100%), Space: 44.3 MB (18.95%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        int fullSum = (n * (n + 1) / 2);
+        int sum = 0;
+        for (int i : nums) {
+            sum += i;
+        }
+        return fullSum - sum;
+    }
+}
